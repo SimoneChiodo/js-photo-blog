77972e8b8da2cfc95a0d940f0c0747881321fa4c
SimoneChiodo <simochi.005@gmail.com>
2024-11-08 12:41:16 +0100

    Milestone 6; add responsive overlay

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // Dichiarazioni
+const body = document.body;
 const postsTable = document.getElementById("posts-table");
 const postsNumber = 6;
 const hidePost = document.querySelectorAll("[data-activate=post-overlay]");
 const overlay = document.getElementById("overlay");
+const overlayImage = document.getElementById("overlay-image");
 let posts = [];
 
 // API per generare Immagini
@@ -12,7 +14,7 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + postsNumber)
         //Genero i post
         data.forEach((element) => {
             // Creo un post per l'immagine
-            createPost(element.url, element.title);
+            createPost(element.id, element.url, element.title);
 
             // Assegno la funzione che un overlay dell'immagine
             if (element.id === postsNumber) activateOverlay();
@@ -21,10 +23,10 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=" + postsNumber)
     .catch((error) => console.error(error));
 
 // Funzione per generare 1 post
-function createPost(img, description) {
+function createPost(id, img, description) {
     postsTable.innerHTML += `
         <!-- Post Card -->
-        <div class="col py-3 container-center">
+        <div id="post-${id}" class="col py-3 container-center">
             <!-- Post Container -->
             <div class="post-container p-3 h-100"> 
                 <!-- Pin Image -->
@@ -51,10 +53,20 @@ function activateOverlay() {
     // Prelevo tutti i post dentro la pagina
     posts = document.querySelectorAll("#posts-table > .col");
 
+    // Aggiungo una funzione per quando clicco un post
     posts.forEach((element) => {
-        // Quando clicco un post: aggiungo l'apertura di un overlay
         element.addEventListener("click", () => {
+            // Apro l'overlay
             overlay.classList.remove("d-none");
+            body.classList.add("overflow-y-hidden");
+
+            //Prelevo l'immagine attuale
+            const actualImage = document.querySelector(
+                `#${element.id} .post-image`
+            ).src;
+
+            // Aggiungo l'immagine all'overlay
+            overlayImage.src = actualImage;
         });
     });
 }
@@ -63,5 +75,6 @@ function activateOverlay() {
 hidePost.forEach((element) => {
     element.addEventListener("click", () => {
         overlay.classList.add("d-none");
+        body.classList.remove("overflow-y-hidden");
     });
 });
